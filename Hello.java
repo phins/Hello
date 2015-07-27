3146848b62ffaf64889cac18c739799a536b0970
phin <psridhar@digitalguardian.com>
2015-07-27 11:25:29 -0400

    three commits

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
 	public static void main (String [] args)
 	{
+		System.out.println("test");
 		System.out.println("Java Hello World");
 		System.out.println("Hello again");
 	}
